c584a211124341adbc27e07efd0c3fc539539d43
Alexa263 <olenkakwiatek@gmail.com>
2026-02-26 20:22:18 +0100

    project structure

---
+++ src/fista_logreg.py
@@ -0,0 +1,22 @@
+class FISTALogisticLasso:
+    """
+    Implementation of Logistic Lasso Regression using FISTA.
+    """
+    
+    def __init__(self, lambda_values=None, max_iter=1000, tol=1e-6):
+        pass
+    
+    def fit(self, X_train, y_train):
+        pass
+    
+    def validate(self, X_valid, y_valid, measure='accuracy'):
+        pass
+    
+    def predict_proba(self, X_test):
+        pass
+    
+    def plot(self, measure):
+        pass
+    
+    def plot_coefficients(self):
+        pass
---
+++ src/missing_schemes.py
@@ -0,0 +1,16 @@
+
+
+def generate_mcar(X, Y, c):
+    pass
+
+
+def generate_mar1(X, Y, c):
+    pass
+
+
+def generate_mar2(X, Y, c):
+    pass
+
+
+def generate_mnar(X, Y, c):
+    pass
